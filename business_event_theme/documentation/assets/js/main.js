a3e8601c0ae345cfadf8c4a90fdd487e8e3b8616
Dana Scheider <dana.scheider@gmail.com>
2015-09-13 15:13:13 -0700

    Template for landing page

---
+++ business_event_theme/documentation/assets/js/main.js
@@ -0,0 +1,20 @@
+!function ($) {
+
+  "use strict";
+
+  var docs = function () {
+
+    
+    // Return all functions
+    return {
+      init: function () {
+              
+      }
+    }
+  }();
+
+  $(function () {
+    // Launch functions
+    docs.init()
+  })
+}(window.jQuery);
