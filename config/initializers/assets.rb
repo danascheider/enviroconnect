fd9519d44de33b2e6ad60636909600bc82e919c8
Dana Scheider <dana.scheider@gmail.com>
2015-09-22 12:35:48 -0700

    Get new site theme set up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( search.js bootstrap_and_overrides.css style.css bootstrap.js )
+Rails.application.config.assets.precompile += %w( search.js application.js jquery-1.11.3.min.js jquery.bxslider.min.js lightbox-2.6.js jquery.nav.js jquery.scrollTo.js html5shiv.js respond.js tabs.js jquery_bxslider.css lightbox.css responsive.css styleGreen.css styleOrange.css bootstrap_and_overrides.css style.css bootstrap.js )
