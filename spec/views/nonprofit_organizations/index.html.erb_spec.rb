8f59352eee3f1331a5566d9fb33eb8c9f35338d3
Dana Scheider <dana.scheider@gmail.com>
2015-10-02 15:38:41 -0700

    Make sure organizations created are valid

--- spec/views/nonprofit_organizations/index.html.erb_spec.rb
+++ spec/views/nonprofit_organizations/index.html.erb_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe "nonprofit_organizations/index", type: :view do
   before(:each) do
     assign(:nonprofit_organizations, [
-      NonprofitOrganization.create!(),
-      NonprofitOrganization.create!()
+      NonprofitOrganization.create!(name: 'Organization 1'),
+      NonprofitOrganization.create!(name: 'Organization 2')
     ])
   end
 
   it "renders a list of nonprofit_organizations" do
     render
+    expect(rendered).to include('Organization 1')
+    expect(rendered).to include('Organization 2')
   end
 end
