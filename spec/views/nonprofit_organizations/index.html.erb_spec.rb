eb696b95b0ca8f6d88496e0e2a409b9a857b5175
Dana Scheider <dana.scheider@gmail.com>
2015-10-02 15:40:28 -0700

    Use FactoryGirl instead of instantiating ActiveRecord model directly

--- spec/views/nonprofit_organizations/index.html.erb_spec.rb
+++ spec/views/nonprofit_organizations/index.html.erb_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "nonprofit_organizations/index", type: :view do
   before(:each) do
-    assign(:nonprofit_organizations, [
-      NonprofitOrganization.create!(name: 'Organization 1'),
-      NonprofitOrganization.create!(name: 'Organization 2')
-    ])
+    assign(:nonprofit_organizations, FactoryGirl.create_list(:nonprofit_organization, 2))
   end
 
   it "renders a list of nonprofit_organizations" do
     render
-    expect(rendered).to include('Organization 1')
-    expect(rendered).to include('Organization 2')
+    expect(rendered).to include(NonprofitOrganization.first.name)
+    expect(rendered).to include(NonprofitOrganization.last.name)
   end
 end
