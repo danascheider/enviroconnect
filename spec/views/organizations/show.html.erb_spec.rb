e11dfeaa1c0e79c4453a39794d6eda4b4c703496
Dana Scheider <dana.scheider@gmail.com>
2015-10-02 16:06:18 -0700

    Use FactoryGirl instead of creating ActiveRecord model directly

--- spec/views/organizations/show.html.erb_spec.rb
+++ spec/views/organizations/show.html.erb_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "organizations/show", type: :view do
   before(:each) do
-    @organization = assign(:organization, Organization.create!(
+    @organization = assign(:organization, FactoryGirl.create(:organization,
       :name => "Name",
       :mission => "Mission",
-      :type => "Type",
       :phone => "Phone",
       :address_1 => "Address 1",
       :address_2 => "Address 2",
       :city => "City",
       :state => "State",
-      :zip => 1
     ))
   end
 
@@ -19,12 +17,10 @@ RSpec.describe "organizations/show", type: :view do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/Mission/)
-    expect(rendered).to match(/Type/)
     expect(rendered).to match(/Phone/)
     expect(rendered).to match(/Address 1/)
     expect(rendered).to match(/Address 2/)
     expect(rendered).to match(/City/)
     expect(rendered).to match(/State/)
-    expect(rendered).to match(/1/)
   end
 end
