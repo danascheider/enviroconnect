80ab5af1af7972b2ec177b46cfb5e63d0e26222f
Dana Scheider <dana.scheider@gmail.com>
2015-10-02 16:03:52 -0700

    Create valid organizations for tests using FactoryGirl and make sure tests look only for attributes shown in the index view

--- spec/views/organizations/index.html.erb_spec.rb
+++ spec/views/organizations/index.html.erb_spec.rb
@@ -2,42 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "organizations/index", type: :view do
   before(:each) do
-    assign(:organizations, [
-      Organization.create!(
-        :name => "Name",
-        :mission => "Mission",
-        :type => "Type",
-        :phone => "Phone",
-        :address_1 => "Address 1",
-        :address_2 => "Address 2",
-        :city => "City",
-        :state => "State",
-        :zip => 1
-      ),
-      Organization.create!(
-        :name => "Name",
-        :mission => "Mission",
-        :type => "Type",
-        :phone => "Phone",
-        :address_1 => "Address 1",
-        :address_2 => "Address 2",
-        :city => "City",
-        :state => "State",
-        :zip => 1
-      )
-    ])
+    assign(:organizations, FactoryGirl.create_list(:organization, 2))
   end
 
   it "renders a list of organizations" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Mission".to_s, :count => 2
-    assert_select "tr>td", :text => "Type".to_s, :count => 2
-    assert_select "tr>td", :text => "Phone".to_s, :count => 2
-    assert_select "tr>td", :text => "Address 1".to_s, :count => 2
-    assert_select "tr>td", :text => "Address 2".to_s, :count => 2
-    assert_select "tr>td", :text => "City".to_s, :count => 2
-    assert_select "tr>td", :text => "State".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    expect(rendered).to include(Organization.first.name)
+    expect(rendered).to include(Organization.last.name)
   end
 end
