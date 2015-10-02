de8ca5d940a40b9bcb53595380e71707bdd21bab
Dana Scheider <dana.scheider@gmail.com>
2015-10-02 15:29:47 -0700

    Add scenario for invalid update

--- features/edit_organization.feature
+++ features/edit_organization.feature
@@ -10,3 +10,10 @@ Feature: Edit organization
   Scenario: Valid update
     When I fill in the 'organization[phone]' field with '(503) 555-1212'
     And I submit the edit form for the organization
+    Then the organization's 'phone' attribute should be '(503) 555-1212'
+
+  Scenario: Invalid update
+    When I fill in the 'organization[zip]' field with 'foobar'
+    And I submit the edit form for the organization
+    Then the organization's 'zip' attribute should not be 'foobar'
+    And it should re-render the organization's edit form
--- features/step_definitions/organization_steps.rb
+++ features/step_definitions/organization_steps.rb
@@ -2,6 +2,10 @@ Given(/^an organization$/) do
   @organization = FactoryGirl.create(:organization)
 end
 
+Given(/^there are (\d+) organizations$/) do |count|
+  FactoryGirl.create_list(:organization, count.to_i)
+end
+
 When(/^I visit the organization's profile$/) do 
   visit "/organizations/#{@organization.id}"
 end
@@ -18,10 +22,6 @@ Then(/^I should see the organization's edit form$/) do
   expect(page).to have_css "form#edit_organization_#{@organization.id}"
 end
 
-Given(/^there are (\d+) organizations$/) do |count|
-  FactoryGirl.create_list(:organization, count.to_i)
-end
-
 Then(/^I should see (\d+) organizations listed$/) do |count|
   expect(page).to have_css '.list-item', count: count.to_i
 end
@@ -31,5 +31,19 @@ Then(/^I should (not )?see its (.*)$/) do |neg, attribute|
     expect(page).not_to have_text(Organization.human_attribute_name(attribute.to_sym))
   else
     expect(page).to have_text(@organization.send(attribute.to_sym))
+  end
+end
+
+Then(/^it should re-render the organization's edit form$/) do
+  step "I should see the organization's edit form"
+end
+
+Then(/^the organization's '(.*)' attribute should (not )?be '(.*)'$/) do |attribute, neg, value|
+  @organization = Organization.where(id: @organization.id).first
+
+  if neg
+    expect(@organization.send(attribute.to_sym)).not_to eql value
+  else
+    expect(@organization.send(attribute.to_sym)).to eql value
   end
 end
