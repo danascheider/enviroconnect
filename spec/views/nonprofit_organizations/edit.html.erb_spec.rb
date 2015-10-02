69904d0a9c32a00f66d9291ed5be021d5d90b989
Dana Scheider <dana.scheider@gmail.com>
2015-10-02 15:28:04 -0700

    Make sure organization generated for test is valid and that assertion doesn't raise error

--- spec/views/nonprofit_organizations/edit.html.erb_spec.rb
+++ spec/views/nonprofit_organizations/edit.html.erb_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "nonprofit_organizations/edit", type: :view do
   before(:each) do
-    @nonprofit_organization = assign(:nonprofit_organization, NonprofitOrganization.create!())
+    @nonprofit_organization = assign(:nonprofit_organization, NonprofitOrganization.create!(name: 'Organization'))
   end
 
   it "renders the edit nonprofit_organization form" do
     render
-
-    assert_select "form[action=?][method=?]", nonprofit_organization_path(@nonprofit_organization), "post" do
-    end
+    assert_select("form[action='/nonprofit_organizations/#{@nonprofit_organization.id}'][method='post']")
   end
 end
