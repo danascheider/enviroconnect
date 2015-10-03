16c17b10ae9c1993b4750d8090c48cf89163a609
Dana Scheider <dana.scheider@gmail.com>
2015-10-02 18:40:13 -0700

    Add examples to nonprofit model spec

--- spec/models/nonprofit_organization_spec.rb
+++ spec/models/nonprofit_organization_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe NonprofitOrganization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has type NonprofitOrganization' do 
+    organization = NonprofitOrganization.create!(name: 'My Organization')
+    expect(organization.type).to eql 'NonprofitOrganization'
+  end
 end
